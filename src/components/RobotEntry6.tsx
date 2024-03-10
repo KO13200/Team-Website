789ed07fa12866a40f7eb52aeacb25832aea1b71
koz <kido3508@gmail.com>
2024-03-10 14:06:44 +0800

    v1

---
+++ src/components/RobotEntry6.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+import styles from "../styles/_robotentry.module.scss";
+
+const RobotEntry: React.FC = () => {
+  const [hoveredRobot, setHoveredRobot] = useState<string | null>(null);
+
+  const handleMouseEnter = (content: string) => {
+    setHoveredRobot(content);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredRobot(null);
+  };
+
+  return (
+    <div className={styles.robotEntry} onMouseEnter={() => handleMouseEnter("Our 2024 robot NOTEDOG MILLIONARE: WIP")} onMouseLeave={handleMouseLeave}>
+      <div className={styles.imageContainer}>
+        <Image src='/5663_2019.jpg' alt="i hate this" width={600} height={336} />
+      </div>
+      {hoveredRobot && (
+        <div className={styles.tooltip}>
+          <div className={styles.tooltipContent}>{hoveredRobot}</div>
+        </div>
+      )}
+    </div>
+    
+    
+  );
+};
+
+export default RobotEntry;
--- src/pages/robots.tsx
+++ src/pages/robots.tsx
@@ -1,9 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import React from 'react';
+import Head from 'next/head';
+import RobotEntry from '../components/RobotEntry';
+import RobotEntry2 from '../components/RobotEntry2'; 
 
-export default function Home() {
+function RobotsPage() {
+  var robots = [
+    {desc: "Our 2019 robot KillSwitch: Designed to be capable of scoring cargo quickly with a small hatch panel 'flooper' this robot has been described as a cargo beast. Competed at: South Pacific Regional - Lost in Semi Finals", image: "/5663_2019.jpg"}
+    ,{desc2: "Our 2020 robot ,TBC", image: "/2020_render.png" }
+  ];
+
   return (
-    <>
+    <div>,
       <Head>
         <title>Robots | 4788</title>
         <meta name="description" content="Curtin FRC's robots" />
@@ -11,17 +18,21 @@ export default function Home() {
       <div className="page">
         <div className='hero-no-padding'>
           <div className="column">
-            <Image src="/5663_2019.jpg" alt="Our 2019 robot KillSwitch" width={600} height={336}/>
-            <p>Our 2019 robot KillSwitch: 
-                  Designed to be capable of scoring cargo quickly with a small hatch panel &apos;flooper&apos; this robot has been described as a cargo beast
-		<br/>
-                  Competed at:
-                  South Pacific Regional - Lost in Semi Finals
-            </p>
+            {robots.map((robot, index) => (
+              <div key={index}>
+                <RobotEntry desc={robot.desc} />
+                <br/><br/><br/><br/>
+              </div>
+            ))}
+            <div>
+              <RobotEntry2  desc2={robots.desc2}/>
+              <br/><br/><br/><br/>
+            </div>
           </div>
         </div>
-        <div className='heros'> </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
+
+export default RobotsPage;
